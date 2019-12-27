fa5df1945b43be81c9c7f601d194fe0741c003cf
Felipe Cabezudo <felipecabedilo@gmail.com>
2019-12-27 15:41:26 -0300

    update

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,11 @@
 # Python 3.8
 import numpy as np
+import collections
 import pickle
 import copy
 
-P1_SYMBOL = "❌"
-P2_SYMBOL = "0️⃣"
+P1_SYMBOL = "1"
+P2_SYMBOL = "2"
 ROWS = 3
 EMPTY_CELL = 0
 COLUMNS = 3
@@ -18,14 +19,15 @@ class TicTacToe:
         self.turn = p1
         self.winner = None
         self.turns = 0
-        calculate_board_hash(self)
+        self.calculate_board_hash()
 
     def calculate_board_hash(self):
         self.board_hash = hash(str(self.board))
 
     def has_ended(self):
-        if winner := winner(self) is not None:
+        if winner:=self.who_win() is not None:
             self.winner = winner
+            print('rewarding')
             self.reward(self.p1, self.p2) if winner == self.p1 else self.reward(self.p2, self.p1)
             return True
         elif self.turns == 9:
@@ -37,59 +39,59 @@ class TicTacToe:
         winner.reward(1)
         loser.reward(0)
 
-    def winner(self):
-        if self.exists_winning_combination(self):
-            return last_player_to_play(self)
+    def who_win(self):
+        if self.exists_winning_combination():
+            return self.last_player_to_play()
         else:
             return None
 
     def last_player_to_play(self):
         if (self.turns % 2 == 0):
-            return p2
+            return self.p2
         else:
-            return p1
+            return self.p1
 
-    def avaliable_positions(self, board=self.board):
+    def avaliable_positions(self):
         positions = []
         for i in range(ROWS):
             for j in range(COLUMNS):
-                if board[i,j] == 0:
+                if self.board[i,j] == 0:
                     positions.append((i,j))
         return positions
 
-    def exists_winning_comination(self):
+    def exists_winning_combination(self):
 
         # Check for Columns combinations
         for i in range(ROWS):
-            if (self.board[i,:] == np.full((COLUMNS), P1_SYMBOL)).all() or (self.board[i,:] == np.full((COLUMNS), P2_SYMBOL)).all():
+            if (self.board[i,:] == np.full(COLUMNS, float(P1_SYMBOL))).all() or (self.board[i,:] == np.full((COLUMNS), float(P2_SYMBOL))).all():
                 return True
 
         # Check for Rows combinations
         for j in range(COLUMNS):
-            if (self.board[:,j] == np.full((ROWS), P1_SYMBOL)).all() or (self.board[:,j] == np.full((ROWS), P2_SYMBOL)).all():
+            if (self.board[:,j] == np.full(ROWS, float(P1_SYMBOL))).all() or (self.board[:,j] == np.full((ROWS), float(P2_SYMBOL))).all():
                 return True
 
         # Check for Diagonals
         for i in range(COLUMNS):
-            if (self.board[i, i] == P1_SYMBOL):
+            if (self.board[i, i] != float(P1_SYMBOL)):
                 break
             if i == COLUMNS - 1:
                 return True
 
         for i in range(COLUMNS):
-            if (self.board[i, i] == P2_SYMBOL):
+            if (self.board[i, i] != float(P2_SYMBOL)):
                 break
             if i == COLUMNS - 1:
                 return True
 
         for i in range(COLUMNS):
-            if (self.board[COLUMNS - i, i] == P1_SYMBOL):
+            if (self.board[COLUMNS - i - 1, i] != float(P1_SYMBOL)):
                 break
             if i == COLUMNS - 1:
                 return True
 
         for i in range(COLUMNS):
-            if (self.board[COLUMNS - i, i] == P2_SYMBOL):
+            if (self.board[COLUMNS - i - 1, i] != float(P2_SYMBOL)):
                 break
             if i == COLUMNS - 1:
                 return True
@@ -98,7 +100,7 @@ class TicTacToe:
 
     def play(self, position):
         self.board[position] = self.turn.symbol
-        self.turn = p1 if self.turn == self.p2 else p1
+        self.turn = self.p1 if self.turn == self.p2 else self.p2
 
 
     def lines(self):
@@ -115,7 +117,7 @@ class TicTacToe:
             print('-------------')
             out = '| '
             for j in range(0, COLUMNS):
-                out += self.board[i,j] + ' | '
+                out = out + str(self.board[i,j]) + ' | '
             print(out)
         print('-------------')
 
@@ -133,8 +135,10 @@ class Player:
         self.name = name
         self.eta = eta
         self.values = np.zeros(6)
-        load_experience(self)
-        load_weights(self)
+        self.weights = np.zeros(6)
+        self.values = np.zeros(6)
+        self.load_experience()
+        self.load_weights()
 
     def set_tic_tac_toe(self, tic_tac_toe):
         self.tic_tac_toe = tic_tac_toe
@@ -143,7 +147,7 @@ class Player:
     def make_action(self, positions, board):
         if np.random.uniform(0, 1) >= self.banana_rate:
             max_val = -float('inf')
-            for k in range(positions):
+            for k in positions:
                 possible_board = board.copy()
                 possible_board[k] = self.symbol
                 possible_board_hash = hash(str(possible_board))
@@ -162,15 +166,10 @@ class Player:
             return value
 
     def reward(self, value):
-        for key in reversed(self.plays):
-            if self.experience.get(key) is None:
-                self.experience[key] = 0
-
-            # Here we should update the values with the evaluations
-            self.experience[key] += self.weight * (value - self.experience[key])
-
-        save_experience(self)
-    # Reinforcement learning ----------------------------------------------------END
+        for i in range(self.experience.size):
+            self.experience[i] += self.weights[i] * (value - self.experience[i])
+        print(self.experience)
+        self.save_experience()
 
 
     # ------------------------------------------------------------------------------------------------ #
@@ -184,26 +183,31 @@ class Player:
     # x5 amount of lines where player 2 needs 2 pieces to win, and there are 2 free spot on that line  #
     # ------------------------------------------------------------------------------------------------ #
 
-    ME_0_TO_WIN = 0
-    ME_1_TO_WIN = 1
-    ME_2_TO_WIN = 2
-    OP_0_TO_WIN = 3
-    OP_1_TO_WIN = 4
-    OP_2_TO_WIN = 5
+
 
     def choose_action(self):
         positions = self.tic_tac_toe.avaliable_positions()
-        for k in range(positions):
-            tic_tac_toe = copy.deepcopy(self.tic_tac_toe)
-            tic_tac_toe.play(k)
-            lines = tic_tac_toe.lines()
-            if value := evaluate(lines) >= max_val:
-                max_val = value
-                play = k
-        return play
+        if positions :
+            max_val=0
+            play = positions[0]
+            for k in positions:
+                tic_tac_toe = copy.deepcopy(self.tic_tac_toe)
+                tic_tac_toe.play(k)
+                lines = tic_tac_toe.lines()
+                if value := self.evaluate(lines) >= max_val:
+                    max_val = value
+                    play = k
+            return play
+        return False
 
     def evaluate(self, lines):
-        calculate_values(lines)
+        ME_0_TO_WIN = 0
+        ME_1_TO_WIN = 1
+        ME_2_TO_WIN = 2
+        OP_0_TO_WIN = 3
+        OP_1_TO_WIN = 4
+        OP_2_TO_WIN = 5
+        self.calculate_values(lines)
         if (self.values[ME_0_TO_WIN] > 0):           # If the plaher has 3 in a line
             return 100                               # WIN
         elif (self.values[OP_0_TO_WIN] > 0):         # If the opponent player has 3 in a line
@@ -213,21 +217,30 @@ class Player:
 
     def calculate_values(self, lines):
         # check for conditions described and assign values to self.values
-        self.values = np.zeros(6)                                                   # RESETS THE VALUES OF THE RANDOM DISCRETE VARIABLES
-        self.values[ME_0_TO_WIN] = sum(map(lambda x : x.count(self.symbol) == 3, lines))
-        self.values[ME_1_TO_WIN] = sum(map(lambda x : x.count(self.symbol) == 2 and x.count(EMPTY_CELL) == 1, lines))
-        self.values[ME_2_TO_WIN] = sum(map(lambda x : x.count(self.symbol) == 1 and x.count(EMPTY_CELL) == 2, lines))
-        self.values[OP_0_TO_WIN] = sum(map(lambda x : x.count(self.symbol) == 0 and x.count(EMPTY_CELL) == 0, lines))
-        self.values[OP_1_TO_WIN] = sum(map(lambda x : x.count(self.symbol) == 0 and x.count(EMPTY_CELL) == 1, lines))
-        self.values[OP_2_TO_WIN] = sum(map(lambda x : x.count(self.symbol) == 0 and x.count(EMPTY_CELL) == 2, lines))
+        ME_0_TO_WIN = 0
+        ME_1_TO_WIN = 1
+        ME_2_TO_WIN = 2
+        OP_0_TO_WIN = 3
+        OP_1_TO_WIN = 4
+        OP_2_TO_WIN = 5
+        self.values = np.zeros(6)
+        self.values[ME_0_TO_WIN] = sum(map(lambda x : collections.Counter(x)[float(self.symbol)] == 3, lines))
+        self.values[ME_1_TO_WIN] = sum(map(lambda x : collections.Counter(x)[float(self.symbol)] == 2 and collections.Counter(x)[EMPTY_CELL]== 1, lines))
+        self.values[ME_2_TO_WIN] = sum(map(lambda x : collections.Counter(x)[float(self.symbol)] == 1 and collections.Counter(x)[EMPTY_CELL]== 2, lines))
+        self.values[OP_0_TO_WIN] = sum(map(lambda x : collections.Counter(x)[float(self.symbol)] == 0 and collections.Counter(x)[EMPTY_CELL]== 0, lines))
+        self.values[OP_1_TO_WIN] = sum(map(lambda x : collections.Counter(x)[float(self.symbol)] == 0 and collections.Counter(x)[EMPTY_CELL]== 1, lines))
+        self.values[OP_2_TO_WIN] = sum(map(lambda x : collections.Counter(x)[float(self.symbol)] == 0 and collections.Counter(x)[EMPTY_CELL]== 2, lines))
 
     def update_weights(self):
-        tic_tac_training = copy.deepcopy(self.tic_tac_toe).play(self.choose_action())
-        v_train = self.evaluate(tic_tac_training)
-        v = self.evaluate(self.tic_tac_toe.lines())
-        for i in range(self.weights):
-            self.weights[i] = self.weights[i] + self.eta*(v_train - v)*self.values[i]
-        save_weights()
+        tic_tac_training = copy.deepcopy(self.tic_tac_toe)
+        if self.choose_action():
+            tic_tac_training.play(self.choose_action())
+            if lines:=tic_tac_training.lines():
+                v_train = self.evaluate(lines)
+                v = self.evaluate(self.tic_tac_toe.lines())
+                for i in range(self.weights.size):
+                    self.weights[i] = self.weights[i] + self.eta*(v_train - v)*self.values[i]
+                self.save_weights()
 
 
 
@@ -236,19 +249,25 @@ class Player:
     # ------------------------------------------------------ #
 
     def load_experience(self):
-        with open('{FILE_NAME}_{self.name}.pkl', 'rb') as f:
-            self.experience = pickle.load(f)
+        with open("{file}_{name}.pkl".format(file='experience', name=self.name), 'rb') as f:
+            try:
+                self.experience = pickle.load(f)
+            except EOFError:
+                self.experience = np.zeros(6)
 
     def save_experience(self):
-        with open('{FILE_NAME}_{self.name}.pkl', 'wb') as f:
+        with open("{file}_{name}.pkl".format(file='experience', name=self.name), 'wb') as f:
             pickle.dump(self.experience, f)
 
     def load_weights(self):
-        with open('{WEIGHTS_FILE}_{self.name}.pkl', 'rb') as f:
-            self.weights = pickle.load(f)
+        with open("{file}_{name}.pkl".format(file='weights', name=self.name), 'rb') as f:
+            try:
+                self.weights = pickle.load(f)
+            except EOFError:
+                self.experience = np.zeros(6)
 
     def save_weights(self):
-        with open('{WEIGHTS_FILE}_{self.name}.pkl', 'wb') as f:
+        with open("{file}_{name}.pkl".format(file='weights', name=self.name), 'wb') as f:
             pickle.dump(self.weights, f)
 
 
@@ -266,27 +285,45 @@ class Judger:
         self.p2.set_tic_tac_toe(tic_tac_toe=self.ttt)
 
     def play(self):
-        self.ttt.turn.play
+        self.ttt.play(self.ttt.turn.choose_action())
+        self.ttt.turns += 1
 
-def train(rounds=200):
-    player1 = Player(symbol= P1_SYMBOL)
-    player2 = Player(symbol= P2_SYMBOL)
+
+def train(rounds=500):
+    player1 = Player(symbol= P1_SYMBOL, name='player_one')
+    player2 = Player(symbol= P2_SYMBOL, name='player_two')
     player1_wins = 0
+    ties = 0
     player2_wins = 0
     judger = Judger(player1=player1, player2=player2)
     for i in range(rounds):
-        judger.play()
-        print('round: ', i)
-        if judger.ttt.has_ended():
-            print('winner: ', judger.ttt.winner().symbol)
-            if judger.ttt.winner().symbol == P1_SYMBOL :
+        while not judger.ttt.has_ended():
+            judger.play()
+            player1.update_weights()
+            player2.update_weights()
+        judger.ttt.show()
+        if judger.ttt.who_win() is not None:
+            print('winner: ', judger.ttt.who_win().symbol)
+            if judger.ttt.who_win().symbol == P1_SYMBOL :
                 player1_wins += 1
-            if judger.ttt.winner().symbol == P2_SYMBOL :
+            if judger.ttt.who_win().symbol == P2_SYMBOL :
                 player2_wins += 1
-            judger.ttt.winner()
-            judger.reset()
+        else:
+            print('tie')
+            ties += 1
+        judger.reset()
+    print('weights', player1.weights)
+    print('weights', player2.weights)
+    print('experience', player1.experience)
+    print('experience', player2.experience)
+    print('player 1 wins:', player1_wins)
+    print('player 2 wins:', player2_wins)
+    print('ties:', ties)
 
     player1.save_experience()
     player1.save_weights()
     player2.save_experience()
     player2.save_weights()
+
+
+train()
